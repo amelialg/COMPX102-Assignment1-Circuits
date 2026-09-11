587227ee3eb45a4a1b5efb7017f6df63fb06ece1
amelialg <amelialg.2025@gmail.com>
2026-09-11 14:39:30 +1200

    Test + Changed the AND gate image 11/9/2026

--- Circuits/AndGate.cs
+++ Circuits/AndGate.cs
@@ -124,13 +124,13 @@ namespace Circuits
 
             // AND is simple, so we can use a circle plus a rectange.
             // An alternative would be to use a bitmap.
-            paper.FillEllipse(brush, left, top, WIDTH, HEIGHT);
-            paper.FillRectangle(brush, left, top, WIDTH / 2, HEIGHT);
+            //paper.FillEllipse(brush, left, top, WIDTH, HEIGHT);
+            //paper.FillRectangle(brush, left, top, WIDTH / 2, HEIGHT);
 
             //Note: You can also use the images that have been imported into the project if you wish,
             //      using the code below.  You will need to space the pins out a bit more in the constructor.
             //      There are provided images for the other gates and selected versions of the gates as well.
-            //paper.DrawImage(Properties.Resources.AndGate, Left, Top);
+            paper.DrawImage(Properties.Resources.AndGate, Left, Top);
 
 
         }
